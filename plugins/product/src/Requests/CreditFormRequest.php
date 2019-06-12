cab0681cfd014122611accfbe8e09efb66964cac
Trinh Le <trinh.le@bigin.vn>
2019-06-12 20:56:26 +0700

    Update UI

--- plugins/product/src/Requests/CreditFormRequest.php
+++ plugins/product/src/Requests/CreditFormRequest.php
@@ -15,19 +15,9 @@ class CreditFormRequest extends CoreRequest
      */
     public function rules()
     {
-        $cardType = implode(',', [
-            PaymentReferenceConfig::REFERENCE_PAYMENT_PAYPAL_CARD_NAME_VISA,
-            PaymentReferenceConfig::REFERENCE_PAYMENT_PAYPAL_CARD_NAME_MASTER,
-            PaymentReferenceConfig::REFERENCE_PAYMENT_PAYPAL_CARD_NAME_DISCOVER,
-            PaymentReferenceConfig::REFERENCE_PAYMENT_PAYPAL_CARD_NAME_AMEX,
-        ]);
-
         return [
             'creditcard'                  => 'required|array',
-            'creditcard.card_name'        => "required|min:3|max:100|in:{$cardType}",
             'creditcard.card_number'      => 'required|min:3|max:255',
-            'creditcard.last_name'        => 'required|min:3|max:255',
-            'creditcard.first_name'       => 'required|min:3|max:255',
             'creditcard.card_cvv'         => 'required|min:3|max:255',
             'creditcard.expiration_year'  => 'required|min:3|max:255',
             'creditcard.expiration_month' => 'required',
